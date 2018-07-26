bec7defc93d746f91d1104db6a815d6c157d99ca
danny <danny@dannys-MacBook-Pro.local>
2018-07-26 12:53:45 -0700

    silly lerp test

---
+++ TT/Assets/LerpTest/LerpCubeDuration.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LerpCubeDuration : MonoBehaviour 
+{   
+    public Vector3 EndPos;
+    public float Duration;
+
+    private float _startTime;
+    private Vector3 _startPos;
+
+	// Use this for initialization
+	void Start() 
+    {
+        _startTime = Time.time;
+        _startPos = transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update() 
+    {
+        float elapsedTime = Time.time - _startTime;
+        if(elapsedTime >= Duration)
+        {
+            transform.position = EndPos;
+        }
+        else
+        {
+            float percent = elapsedTime / Duration;
+            transform.position = lerp(_startPos, EndPos, percent);
+        }
+	}
+
+    Vector3 lerp(Vector3 start, Vector3 end, float percent)
+    {
+        return start + ((end - start) * percent);
+    }
+}
---
+++ TT/Assets/LerpTest/LerpCubeSpeed.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LerpCubeSpeed : MonoBehaviour 
+{   
+    public Vector3 EndPos;
+    public float Speed;
+
+    private float _distance;
+    private Vector3 _dir;
+    private Vector3 _startPos;
+
+
+	// Use this for initialization
+	void Start() 
+    {        
+        _startPos = transform.position;
+        _dir = EndPos - _startPos;
+        _distance = Vector3.Distance(EndPos, _startPos);
+	}
+	
+	// Update is called once per frame
+	void Update() 
+    {
+        Vector3 newPos = transform.position + (_dir * Speed * Time.deltaTime);
+        float distTraveled = Vector3.Distance(newPos, _startPos);
+        if(distTraveled >= _distance)
+        {
+            transform.transform.position = EndPos;
+        }
+        else
+        {
+            transform.position = newPos;               
+        }
+	}
+}
